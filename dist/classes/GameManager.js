080074c5c66a36ac408cd3f723a930b5323fc9e5
unknown <chen572@gmail.com>
2020-08-07 12:00:48 +0300

    moved all game logic to server and added routes

--- dist/classes/GameManager.js
+++ dist/classes/GameManager.js
@@ -1,9 +1,22 @@
 export class GameManager {
     constructor() {
         this.gameBoard = []
+        this.playing = false
+        this.winner = null
+        this.playerOneScore = 0
+        this.playerTwoScore = 0
     }
 
-    async initGameBoard(x, y) { this.gameBoard = await $.get(`/board?state=new&x=${x}&y=${y}`) }
+    async initGameBoard(x, y) { this.gameBoard = await $.post(`/board?x=${x}&y=${y}`); this.playing = true }
 
-    async refreshGameBoard() { this.gameBoard = await $.get('/board?state=old') }
+    async refreshGameBoard() { this.gameBoard = await $.get('/board') }
+
+    async movePlayer(player, direction) { 
+        const gameObj = await $.ajax({ url: `/board?player=${player}&direction=${direction}`, method: 'PUT' }) 
+        gameObj.winner ?
+        this.winner = gameObj.winner :
+        this.gameBoard = gameObj.board
+        this.playerOneScore = gameObj.score.playerOne
+        this.playerTwoScore = gameObj.score.playerTwo
+    }
 }
